09606bf0957ed20192c9b13da91215d6d222e300
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-12 14:18:42 -0700

    refactored fetchingBreedDesc and added test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
 const baseUrl = 'https://api.thecatapi.com/v1';
 const errBaseUrl = 'https://api.thecatapi.co/v1';
 
-const breed = args[0];
-//breeds
-request.get(`${baseUrl}/breeds/search?q=${breed}`, (err, res, body) => {
-  if (err) return console.log(err);
-  const data = JSON.parse(body);
-  if (!data.length) {
-    return console.log('not found');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breedName, cb) => {
+  request.get(`${baseUrl}/breeds/search?q=${breedName}`, (err, res, body) => {
+    if (err) return cb(err, null);
+
+    const data = JSON.parse(body);
+
+    if (!data.length) {
+      return cb('not found', null);
+    } else {
+      return cb(null, data[0].description);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
